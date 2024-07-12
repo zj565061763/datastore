1497fcd3596626c4d4162bf25e649a0d3b96b74f
zjun <565061763@qq.com>
2024-07-12 09:57:18 +0800

    1.0.0-alpha05

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "datastore"
-val libVersion = "1.0.0-alpha04"
+val libVersion = "1.0.0-alpha05"
 
 android {
     namespace = "com.sd.lib.datastore"
