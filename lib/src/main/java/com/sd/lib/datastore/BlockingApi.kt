fcc5a5852455b9f9f8ab4eeb55885bc4b46d61ae
zjun <565061763@qq.com>
2024-10-12 03:06:40 +0800

    blocking

--- lib/src/main/java/com/sd/lib/datastore/BlockingApi.kt
+++ lib/src/main/java/com/sd/lib/datastore/BlockingApi.kt
@@ -6,25 +6,19 @@ import kotlinx.coroutines.runBlocking
  * [DatastoreApi.get]
  */
 fun <T> DatastoreApi<T>.getBlocking(): T? {
-   return runBlocking {
-      get()
-   }
+   return runBlocking { get() }
 }
 
 /**
  * [DatastoreApi.replace]
  */
 fun <T> DatastoreApi<T>.replaceBlocking(transform: suspend (T?) -> T?): T? {
-   return runBlocking {
-      replace(transform)
-   }
+   return runBlocking { replace(transform) }
 }
 
 /**
  * [DatastoreApi.update]
  */
 fun <T> DatastoreApi<T>.updateBlocking(transform: suspend (T) -> T): T? {
-   return runBlocking {
-      update(transform)
-   }
+   return runBlocking { update(transform) }
 }
