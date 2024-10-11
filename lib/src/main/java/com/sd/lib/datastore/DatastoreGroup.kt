a3153e98fd91bb72ce3fb4866d1137c8054edc90
zjun <565061763@qq.com>
2024-10-12 03:56:12 +0800

    format

--- lib/src/main/java/com/sd/lib/datastore/DatastoreGroup.kt
+++ lib/src/main/java/com/sd/lib/datastore/DatastoreGroup.kt
@@ -1,6 +1,7 @@
 package com.sd.lib.datastore
 
 import java.io.File
+import java.security.MessageDigest
 
 internal interface DatastoreGroup {
    fun <T> api(clazz: Class<T>): DatastoreApi<T>
@@ -57,4 +58,15 @@ private class DatastoreGroupImpl(
       val api: DatastoreApi<T>,
       val clazz: Class<T>,
    )
+}
+
+private fun fMd5(input: String): String {
+   val md5Bytes = MessageDigest.getInstance("MD5").digest(input.toByteArray())
+   return buildString {
+      for (byte in md5Bytes) {
+         val hex = Integer.toHexString(0xff and byte.toInt())
+         if (hex.length == 1) append("0")
+         append(hex)
+      }
+   }
 }
