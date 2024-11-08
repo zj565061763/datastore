2f417b261608e5e15042fa6e4e53ea804d266d50
zjun <565061763@qq.com>
2024-11-08 22:27:57 +0800

    sample

--- app/src/main/java/com/sd/demo/datastore/App.kt
+++ app/src/main/java/com/sd/demo/datastore/App.kt
@@ -1,7 +1,6 @@
 package com.sd.demo.datastore
 
 import android.app.Application
-import android.util.Log
 import com.sd.lib.datastore.FDatastore
 
 class App : Application() {
@@ -10,7 +9,7 @@ class App : Application() {
       FDatastore.init(
          context = this,
          onError = {
-            logMsg { Log.getStackTraceString(it) }
+            logMsg { it.stackTraceToString() }
          }
       )
    }
